85419d725f5d6af5cf61edc61ee58685724e369a
Amanda <amandaveras1999@gmail.com>
2021-12-14 15:16:27 -0300

    Iniciando projeto 09

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import ReactDOM from "react-dom";
+import React from 'react';
+
+
+function App() {
+  
+  return (
+    <>
+      <h1>oi</h1>
+    </>
+  );
+}
+
+ReactDOM.render(<App />, document.querySelector(".root"));
